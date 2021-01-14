85725403d4b4c0b0103a5fc2ef6b1a4ac6da8bf6
wangwei <532300391@qq.com>
2021-01-14 09:37:07 +0800

    添加color 的支持

--- src/components/Charts/LineEchart.tsx
+++ src/components/Charts/LineEchart.tsx
@@ -2,14 +2,57 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import EchartCore from './EchartBase'
 import store from 'store'
-import { Spin } from 'antd'
-import { LoadingOutlined } from '@ant-design/icons'
+import { message } from 'antd'
 
-const antLoadingIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />
-
-import { GirdLayoutItemConfig } from '../ReactGridItem'
+import { GirdLayoutItemConfig } from '../ReactGridItem/index'
 import Loading from '../Loading'
 // https://echarts.apache.org/next/examples/en/index.html#chart-type-line
+const lineEchartSampleData1 = {
+  tooltip: {
+    trigger: 'axis',
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true,
+  },
+  xAxis: {
+    type: 'category',
+    boundaryGap: false,
+    data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+  },
+  yAxis: {
+    type: 'value',
+  },
+  series: [
+    {
+      name: '邮件营销',
+      type: 'line',
+      data: [120, 132, 101, 134, 90, 230, 210],
+    },
+    {
+      name: '联盟广告',
+      type: 'line',
+      data: [220, 182, 191, 234, 290, 330, 310],
+    },
+    {
+      name: '视频广告',
+      type: 'line',
+      data: [150, 232, 201, 154, 190, 330, 410],
+    },
+    {
+      name: '直接访问',
+      type: 'line',
+      data: [320, 332, 301, 334, 390, 330, 320],
+    },
+    {
+      name: '搜索引擎',
+      type: 'line',
+      data: [820, 932, 901, 934, 1290, 1330, 1320],
+    },
+  ],
+}
 
 const lineEchartSampleData = {
   xAxisData: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
@@ -23,8 +66,8 @@ const lineEchartSampleData = {
 interface Props {
   config: GirdLayoutItemConfig
 }
-const gChartConfig = (serverData: any) => {
-  const chartData = {
+const gChartConfig = (serverData: any, otherConfigs?: any) => {
+  let chartData = {
     tooltip: {
       trigger: 'axis',
     },
@@ -43,6 +86,9 @@ const gChartConfig = (serverData: any) => {
       type: 'value',
     },
     series: [],
+  }
+  if (otherConfigs) {
+    chartData = Object.assign({}, chartData, otherConfigs)
   }
   const { xAxisData, yAxisData } = serverData
   chartData.xAxis.data = xAxisData
@@ -82,15 +128,22 @@ const LineEchartComponent = (props: Props) => {
     (config && config.height) || undefined,
   ])
   useEffect(() => {
+    console.log('---config change', config)
     if (config) {
-      const { reqMethod, API, width = 500, height = 400 } = config
+      const { reqMethod, API, width = 500, height = 400, color } = config
       if (width) {
         setWidth(width)
       }
       if (height) {
         setHeight(height - 50)
       }
-      if (reqMethod && API && !isRequested) {
+      let needRefresh = reqMethod && API && !isRequested
+      console.log(' ---- cur-env ', process.env.NODE_ENV)
+      if (process.env.NODE_ENV === 'development') {
+        // dev env refresh every time
+        needRefresh = true
+      }
+      if (needRefresh) {
         setChartConfig(null)
         axios
           .request({
@@ -101,26 +154,39 @@ const LineEchartComponent = (props: Props) => {
               assetid: store.get('assetid') || '',
             },
           })
-          .then((res) => {
-            // console.log(' res ', res)
-            if (res.status === 200 && res.data) {
-              const data = res.data.data
-              const config = gChartConfig(data)
-              const resultConfig = {
-                config,
-                height,
-                width,
+          .then(
+            (res) => {
+              if (res.status === 200 && res.data && res.data.code === 200) {
+                const data = res.data.data
+                const otherConfigs = {}
+                if (color) {
+                  const colorArr = color.split(',')
+                  otherConfigs.color = colorArr
+                }
+                const chartTypeConfig = gChartConfig(data, otherConfigs)
+                const resultConfig = {
+                  config: chartTypeConfig,
+                  height,
+                  width,
+                }
+                setIsReq(true)
+                setChartConfig(resultConfig)
+              } else {
+                if (res.data && res.data.msg) {
+                  message.error('表格请求数据错误: ', res.data.msg)
+                }
               }
-              setIsReq(true)
-              setChartConfig(resultConfig)
-            }
-          })
+            },
+            (err) => {
+              console.log('error', err.message)
+              message.error(err?.message)
+            },
+          )
       }
     } else {
-      const config = gChartConfig(lineEchartSampleData)
-      console.log(config)
+      const chartTypeConfig = gChartConfig(lineEchartSampleData)
       const resultConfig = {
-        config,
+        config: chartTypeConfig,
         width,
         height,
       }
